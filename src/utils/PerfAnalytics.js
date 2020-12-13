0f903dc8fc811d6f50b4059651aa963ebd56ee92
cihangir mercan <laair24426@gmail.com>
2020-12-13 23:47:37 +0300

    Send data when clicked from clicked a link

--- src/utils/PerfAnalytics.js
+++ src/utils/PerfAnalytics.js
@@ -1,24 +1,22 @@
-import {getFCP, getTTFB} from "web-vitals";
+import {getFCP} from "web-vitals";
 import axios from "axios";
 
 export function sendPerformanceAnalytics() {
-    let fcp;
-    let ttfb;
-    getFCP((fcpEntry) => {
-        fcp = fcpEntry.value;
-        getTTFB((ttfbEntry) => {
-            ttfb = ttfbEntry.value;
-
-            const domLoad = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-            const windowLoad = window.performance.timing.domComplete - window.performance.timing.navigationStart;
-
-            let networkTimings = [];
-            window.performance.getEntriesByType('resource').forEach((item) => {
-                networkTimings.push({
-                    name: item.name,
-                    duration: item.duration
-                })
+    window.addEventListener('load', () => {
+        const ttfb = window.performance.timing.responseStart - window.performance.timing.requestStart;
+        const domLoad = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+        const windowLoad = window.performance.timing.domComplete - window.performance.timing.navigationStart;
+        const networkTimings = [];
+        window.performance.getEntriesByType('resource').forEach((item) => {
+            networkTimings.push({
+                name: item.name,
+                duration: item.duration
             })
+        })
+
+        // Measure and log FCP as soon as it's available.
+        getFCP((rep) => {
+            const fcp = rep.value;
 
             // Send a POST request
             const API_URL = "https://perfanalytics-backend.herokuapp.com/analytics";
