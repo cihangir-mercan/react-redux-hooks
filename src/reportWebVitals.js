5e33964c4b72224a3f889537405032a847b91700
cihangir mercan <laair24426@gmail.com>
2020-12-18 19:55:13 +0300

    Refactor PerfAnalytics.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,29 @@
-import {sendPerformanceAnalytics} from "./utils/PerfAnalytics";
+import axios from "axios";
+import {getPerformanceAnalytics} from "./utils/PerfAnalytics";
 
 const reportWebVitals = () => {
-    sendPerformanceAnalytics();
+    window.addEventListener('load', () => {
+
+        const analytics = getPerformanceAnalytics();
+
+        // Send a POST request
+        const API_URL = "https://perfanalytics-backend.herokuapp.com/analytics";
+        axios({
+            method: 'post',
+            url: API_URL,
+            data: {
+                "ttfb": analytics.ttfb,
+                "fcp": analytics.fcp,
+                "domLoad": analytics.domLoad,
+                "windowLoad": analytics.windowLoad,
+                "networkTimings": analytics.networkTimings
+            }
+        }).then((res) => {
+            console.log("performance analytics are send to the api")
+        }).catch((err) => {
+            console.log("error while sending performance analytics to the api")
+        });
+    });
 };
 
 export default reportWebVitals;
--- src/utils/PerfAnalytics.js
+++ src/utils/PerfAnalytics.js
@@ -1,42 +1,24 @@
-import axios from "axios";
 
-export function sendPerformanceAnalytics() {
-    window.addEventListener('load', () => {
-        const ttfb = window.performance.timing.responseStart - window.performance.timing.requestStart;
-        const domLoad = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-        const windowLoad = window.performance.timing.domComplete - window.performance.timing.navigationStart;
-        const networkTimings = [];
-        window.performance.getEntriesByType('resource').forEach((item) => {
-            networkTimings.push({
-                name: item.name,
-                duration: item.duration
-            })
+export function getPerformanceAnalytics() {
+    const ttfb = window.performance.timing.responseStart - window.performance.timing.requestStart;
+    const domLoad = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    const windowLoad = window.performance.timing.domComplete - window.performance.timing.navigationStart;
+    const networkTimings = [];
+    window.performance.getEntriesByType('resource').forEach((item) => {
+        networkTimings.push({
+            name: item.name,
+            duration: item.duration
         })
+    })
 
-        let fcp = 0;
-        let performanceEntries = performance.getEntriesByType('paint');
-        performanceEntries.forEach((performanceEntry, i, entries) => {
-            if (performanceEntry.name === "first-contentful-paint") {
-                fcp = performanceEntry.startTime;
-            }
-        });
-
-        // Send a POST request
-        const API_URL = "https://perfanalytics-backend.herokuapp.com/analytics";
-        axios({
-            method: 'post',
-            url: API_URL,
-            data: {
-                "ttfb": ttfb,
-                "fcp": fcp,
-                "domLoad": domLoad,
-                "windowLoad": windowLoad,
-                "networkTimings": networkTimings
-            }
-        }).then((res) => {
-            console.log("performance analytics are send to the api")
-        }).catch((err) => {
-            console.log("error while sending performance analytics to the api")
-        });
+    let fcp = 0;
+    let performanceEntries = performance.getEntriesByType('paint');
+    performanceEntries.forEach((performanceEntry, i, entries) => {
+        if (performanceEntry.name === "first-contentful-paint") {
+            fcp = performanceEntry.startTime;
+        }
     });
+
+    return [{"ttfb": ttfb, "fcp": fcp, "domLoad": domLoad, "windowLoad": windowLoad, "networkTimings": networkTimings}];
 }
+
